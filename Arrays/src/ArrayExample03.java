1cdc3c0b40c3c398b43152583257774a41012fd4
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-01-13 13:38:33 +0300

    homework added

--- Arrays/src/ArrayExample03.java
+++ Arrays/src/ArrayExample03.java
@@ -6,6 +6,15 @@ sonrasında ise ortalamasını da hesaplayan ekrana yazdıran java kodunu yazın
 
  */
     public static void main(String[] args) {
-        
+
+        int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
+        int toplam = 0;
+
+        for (int i :numbers){
+            toplam = toplam+i; // toplam += i;
+        }
+        double ortalama= (double) toplam / numbers.length;
+        System.out.println("Toplam:"+ toplam);
+        System.out.println("Ort:"+ ortalama);
     }
 }
