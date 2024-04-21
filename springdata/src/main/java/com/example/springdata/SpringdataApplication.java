1cba6351adb470ed4795f8f06f65d2d17e3b3f5b
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-04-21 14:08:01 +0300

    function sql added

--- springdata/src/main/java/com/example/springdata/SpringdataApplication.java
+++ springdata/src/main/java/com/example/springdata/SpringdataApplication.java
@@ -23,9 +23,10 @@ public class SpringdataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		test();
 		//findAllEmployee();
-	//	findEmployeeById();
-		updateEmployeeById();
+		//	findEmployeeById();
+		//	updateEmployeeById();
         /*Employee employee = new Employee();
 		employee.setAddress("ankara etimesgut");
 		employee.setSurname("ronaldo");
@@ -46,7 +47,7 @@ public class SpringdataApplication implements CommandLineRunner {
 		Optional<Employee> employee = employeeRepository.findById(512L);
 		//employee.ifPresent(System.out::println);
 		Employee e = employee.orElse(new Employee());
-	//	Employee e = employee.orElse(null);
+		//	Employee e = employee.orElse(null);
 
 		System.out.println(e);
 	}
@@ -59,5 +60,10 @@ public class SpringdataApplication implements CommandLineRunner {
 		employee.setFirstname("cristiano");
 		employee.setAge(29);
 		employeeRepository.save(employee);
+	}
+
+	public void test() {
+		List<Employee> employeeList = employeeRepository.findEmployeeByFirstnameAndSurname("c", "b");
+		System.out.println(employeeList.toString());
 	}
 }
--- springdata/src/main/java/com/example/springdata/repository/EmployeeRepository.java
+++ springdata/src/main/java/com/example/springdata/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.example.springdata.repository;
 
 import com.example.springdata.model.Employee;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findEmployeeByFirstnameAndSurname(String firstname, String surname);
 }
