e5abcca43e4e747a38eea1f6b884d8e471e757a6
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-06-30 10:51:17 +0300

    added log Slf4j

--- rresttemplate/src/main/java/com/recap/springboot/rresttemplate/service/PlaceHolderService.java
+++ rresttemplate/src/main/java/com/recap/springboot/rresttemplate/service/PlaceHolderService.java
@@ -1,8 +1,10 @@
 package com.recap.springboot.rresttemplate.service;
 
 import com.recap.springboot.rresttemplate.dto.Post;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -13,22 +15,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class PlaceHolderService {
+
+    private static final String PLACE_HOLDER_ALL_URI = "https://jsonplaceholder.typicode.com/posts";
 
     @Autowired
     private RestTemplate restTemplate;
 
     public List<Post> getAllPostList() {
         HttpEntity<?> entity = getStringHttpEntity(null);
-        return restTemplate.exchange(
-                "https://jsonplaceholder.typicode.com/posts", HttpMethod.GET, entity, new ParameterizedTypeReference<List<Post>>(){}).getBody();
+        log.info("fetching all post list");
+        List<Post> postList = new ArrayList<>();
+        try {
+            postList = restTemplate.exchange(
+                    PLACE_HOLDER_ALL_URI , HttpMethod.GET, entity, new ParameterizedTypeReference<List<Post>>(){}).getBody();
+        }catch (Exception e) {
+            log.error("fetching all post failed for {} URI , Exception {}", PLACE_HOLDER_ALL_URI, e.getMessage());
+        }
+        return postList;
     }
 
     public Post createPost(Post post) {
+        log.info("creating post body = {}", post);
         HttpEntity<?> entity = getStringHttpEntity(post);
-
-        return restTemplate.exchange(
-                "https://jsonplaceholder.typicode.com/posts", HttpMethod.POST, entity, Post.class).getBody();
+        Post createdPost = null;
+        try{
+            createdPost = restTemplate.exchange(
+                     PLACE_HOLDER_ALL_URI, HttpMethod.POST, entity, Post.class).getBody();
+        }catch (Exception e) {
+            log.error("creating post failed for {} URI , body {}", PLACE_HOLDER_ALL_URI, post);
+        }
+        return createdPost;
     }
 
     private HttpEntity<?> getStringHttpEntity(Post post) {
